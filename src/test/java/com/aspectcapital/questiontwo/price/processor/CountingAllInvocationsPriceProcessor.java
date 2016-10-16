569a1177d52724dc3fdfe588a0aadae18776908a
Ewa <imprezka88@gmail.com>
2016-10-16 19:40:56 +0200

    Initial commit

---
+++ src/test/java/com/aspectcapital/questiontwo/price/processor/CountingAllInvocationsPriceProcessor.java
@@ -0,0 +1,22 @@
+package com.aspectcapital.questiontwo.price.processor;
+
+import java.math.BigDecimal;
+import java.util.concurrent.CountDownLatch;
+
+public class CountingAllInvocationsPriceProcessor extends DelayingPriceProcessor {
+    public CountingAllInvocationsPriceProcessor(int processSleep, CountDownLatch latch) {
+        super(processSleep);
+        this.latch = latch;
+    }
+
+    @Override
+    public BigDecimal process(BigDecimal price) {
+        BigDecimal processed = super.process(price);
+
+        if (latch != null) {
+            latch.countDown();
+        }
+
+        return processed;
+    }
+}
